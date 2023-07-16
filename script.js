63f2fc7f990131068fe15b8fd5784c15056a1651
Erick Tolentino <ericktolentino@pursuit.org>
2023-07-16 00:56:09 -0400

    finished

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-const url = "https://opentdb.com/api.php?amount=10"
+const url = "https://opentdb.com/api.php?amount=10";
 const button = document.querySelector("button");
-const centered = document.querySelector(".centered")
+const centered = document.querySelector(".centered");
 
 button.addEventListener("click", (e) => {
-    e.preventDefault()
-   
-    getData()
-    
-})
+  e.preventDefault();
+  getData();
+});
+
 function getData() {
-    fetch(url).then(data => data.json()).then(json => {
-        console.log(json)
-        
-        for(i=0; i < json.results.length ; i++) {
-        centered.innerHTML += `<article class="card">
-        <h2>${json.results[i].category}</h2>
-        <p>${json.results[i].question}</p>
-        <button>Show Answer</button>
-        <p "class="hidden"${json.results[i].correct_answer}</p>
-        </article>`
-        const answerButton = document.querySelector("button")
-        answerButton.addEventListener("click", (e) => {
-            centered.innerHTML = `<article class="card">
-            <p >${json.results[i].correct_answer}</p>`
-    })
+  fetch(url)
+    .then((data) => data.json())
+    .then((json) => {
+      console.log(json);
+
+      for (let i = 0; i < json.results.length; i++) {
+        const card = document.createElement("article");
+        const question = document.createElement("p");
+        const answerButton = document.createElement("button");
+        const answer = document.createElement("p");
+
+        card.classList.add("card");
+        question.innerText = json.results[i].question;
+        answerButton.innerText = "Show Answer";
+        answerButton.addEventListener("click", () => {
+          answer.innerText = json.results[i].correct_answer;
+        });
+
+        card.appendChild(question);
+        card.appendChild(answerButton);
+        card.appendChild(answer);
+        centered.appendChild(card);
+      }
+    });
 }
-    }
-)}
